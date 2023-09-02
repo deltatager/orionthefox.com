56e0c95d9840c2b0ff7fbc4ad9e8bd9094619621
Simon Longpré-Landry <simonllandry@hotmail.com>
2023-09-02 08:17:17 -0400

    Finalized layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,49 +1,59 @@
 <script>
-	import Header from './Header.svelte';
-	import './styles.css';
+  import github from '$lib/images/github.svg'
+  import './styles.css'
 </script>
 
 <div class="app">
-	<Header />
+  <main>
+    <slot />
+  </main>
 
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		&copy; Orion the Astrology fox, 2023
-	</footer>
+  <footer>
+    &copy; Orion the Astrology fox, 2023
+    <a href="https://github.com/deltatager/orionthefox.com" target="_blank">
+      <img src={github} alt="GitHub" />
+    </a>
+  </footer>
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
+  img {
+    width: 2em;
+    height: 2em;
+    object-fit: contain;
+  }
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  a:hover {
+    color: var(--color-theme-1);
+  }
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
 
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    max-width: 64rem;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+
+  footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 12px;
+  }
+
+  @media (min-width: 480px) {
+    footer {
+      padding: 12px 0;
+    }
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,78 +8,73 @@
   <meta name="description" content="Orion's social media links" />
 </svelte:head>
 
-<main>
-  <div>
-    <a href="https://twitter.com/WeaselDumb" target="_blank">
-      <img id='sticker' src={sticker} alt="Orion wink" />
-      by @WeaselDumb
-    </a>
-  </div>
-  <div>
-    <span>Orion the astrology fox's social medias</span>
-  </div>
-  <section>
-    <Qr
-      data={'https://t.me/orionthefox'}
-      label={'Telegram: @orionthefox'}
-      image={'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/128px-Telegram_logo.svg.png'}
-      dotsOptions={{
-        type: 'rounded',
-        color: '#289fd9'
-      }}
-    />
-    <Qr
-      data={'https://www.instagram.com/orion.the.astro.fox'}
-      label={'Instagram: @orion.the.astro.fox'}
-      image={'https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg'}
-      dotsOptions={{
-        type: 'dots',
-        gradient: {
-          type: 'linear',
-          rotation: -45,
-          colorStops: [
-            {
-              offset: 0,
-              color: '#fb5'
-            },
-            {
-              offset: 0.1,
-              color: '#fb5'
-            },
-            {
-              offset: 0.5,
-              color: '#ff543e'
-            },
-            {
-              offset: 1,
-              color: '#c837ab'
-            }
-          ]
-        }
-      }}
-    />
-    <Qr
-      data={'https://twitter.com/orionthefox'}
-      label={'Twitter: @orionthefox'}
-      image={'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/292px-Logo_of_Twitter.svg.png'}
-      dotsOptions={{
-        type: 'rounded',
-        color: '#1d9bf0'
-      }}
-    />
-  </section>
-</main>
+<section>
+  <a class="img-link" href="https://twitter.com/WeaselDumb" target="_blank">
+    <img id="sticker" src={sticker} alt="Orion wink" width="300" />
+    <span  class="img-credit">by @WeaselDumb</span>
+  </a>
+</section>
+<h1>Orion the Astrology fox</h1>
+<section>
+  <Qr
+    data={'https://t.me/orionthefox'}
+    label={'Telegram: @orionthefox'}
+    image={'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/128px-Telegram_logo.svg.png'}
+    dotsOptions={{
+      type: 'rounded',
+      color: '#289fd9'
+    }}
+  />
+  <Qr
+    data={'https://www.instagram.com/orion.the.astro.fox'}
+    label={'Instagram: @orion.the.astro.fox'}
+    image={'https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg'}
+    dotsOptions={{
+      type: 'dots',
+      gradient: {
+        type: 'linear',
+        rotation: -45,
+        colorStops: [
+          {
+            offset: 0,
+            color: '#fb5'
+          },
+          {
+            offset: 0.1,
+            color: '#fb5'
+          },
+          {
+            offset: 0.5,
+            color: '#ff543e'
+          },
+          {
+            offset: 1,
+            color: '#c837ab'
+          }
+        ]
+      }
+    }}
+  />
+  <Qr
+    data={'https://twitter.com/orionthefox'}
+    label={'Twitter: @orionthefox'}
+    image={'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/292px-Logo_of_Twitter.svg.png'}
+    dotsOptions={{
+      type: 'rounded',
+      color: '#1d9bf0'
+    }}
+  />
+</section>
 
 <style>
-
   #sticker {
     display: block;
-    max-width: 300px;
   }
 
-  main {
+  * {
     text-align: center;
   }
+
   section {
     display: flex;
     justify-content: center;
@@ -87,7 +82,15 @@
     flex: 0.7;
   }
 
-  @media(max-width: 960px) {
+  .img-link {
+    text-align: left;
+  }
+
+.img-credit {
+  padding-left: 45px;
+}
+
+  @media (max-width: 960px) {
     section {
       flex-direction: column;
     }
