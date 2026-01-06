bfab632fb9e63ac619f42bb06b3e442da3855447
Simon Longpré-Landry <simonllandry@hotmail.com>
2026-01-05 20:06:16 -0500

    Update QR code image source URL

--- src/routes/QrCodes.svelte
+++ src/routes/QrCodes.svelte
@@ -15,7 +15,7 @@
   <QrCode
   data={'https://x.com/orionthefox'}
   label={'X: @orionthefox'}
-  image={'https://commons.wikimedia.org/wiki/File:X-Logo-Round-Color.png#/media/File:X_logo_2023_original.svg'}
+  image={'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/X_logo_2023_original.svg/960px-X_logo_2023_original.svg.png'}
   dotsOptions={{
     type: 'rounded',
     color: '#999'
